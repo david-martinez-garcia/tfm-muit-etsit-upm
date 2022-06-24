a1e61fda47134425f890d5b18f5c58a42d4eb13d
David Martínez García <94689492+martinezgarciadavid@users.noreply.github.com>
2022-06-24 19:04:23 +0200

    Update download_unpack_scenarios.sh

--- Escenarios de VNX/Otros scripts/download_unpack_scenarios.sh
+++ Escenarios de VNX/Otros scripts/download_unpack_scenarios.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 
-echo 'Se descargarán y desempaquetarán los escenarios de VNX'
+echo "Se descargarán y desempaquetarán los escenarios de VNX"
 
-echo ''
-echo ''
+echo ""
+echo ""
 
-echo 'Se creará un directorio para almacenar los escenarios en: $(pwd)/escenarios_vnx_completos'
+echo "Se creará un directorio para almacenar los escenarios en: $(pwd)/escenarios_vnx_completos"
 
-echo ''
-echo ''
+echo ""
+echo ""
 
 mkdir -p escenarios_vnx_completos
 
-sudo wget https://github.com/martinezgarciadavid/tfm-muit-etsit-upm/releases/download/v1.0.0/escenarios_vnx_completos.tar.gz -P $(pwd)/escenarios_vnx_completos
+wget https://github.com/martinezgarciadavid/tfm-muit-etsit-upm/releases/download/v1.0.0/escenarios_vnx_completos.tar.gz -P $(pwd)/escenarios_vnx_completos
 
 cd $(pwd)/escenarios_vnx_completos
 
-sudo tar -xvzpf escenarios_vnx_completos.tar.gz
+tar -xvzpf escenarios_vnx_completos.tar.gz
 
-sudo rm -Rf *.tar.gz
+rm -Rf *.tar.gz
 
-echo ''
-echo ''
+echo ""
+echo ""
 
-echo 'Completado'
+echo "Completado"
